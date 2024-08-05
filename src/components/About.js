2bb9282c095fe47cd56ec5ded9430a333cc208f6
Duncan <njorogedun870@gmail.com>
2024-08-05 21:01:28 +0300

    resolved all errors and passed all tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,37 @@
 import React from "react";
 
-function About() {
+function BioInfo (props){
+ const Item = props.bio;
+ if(Item){
+   return <p>{Item}</p>;
+ } else {
+   return "Bio not provided";
+ 
+}
+}
+
+function Links(props)  {
+  
+  return(<div>
+    <h3>Links</h3>
+    <a href={props.github}>{props.github}</a>
+    <a href={props.linkedin} >{props.linkedin}</a>
+  </div>);
+}
+ 
+
+
+function About(props) {
+  // const bio = "I made this";
+  const github = "https://github.com/liza";
+  const linkedin = "https://www.linkedin.com/in/liza/";
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <BioInfo bio={props.bio} />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
